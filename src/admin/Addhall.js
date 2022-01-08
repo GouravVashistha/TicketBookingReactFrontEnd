1d8d7da10d47161c161b5d6eb8d2d071c1c3486f
Gourav Vashistha <59836501+GouravVashistha@users.noreply.github.com>
2022-01-08 13:48:25 +0530

    Ten commit

--- src/admin/Addhall.js
+++ src/admin/Addhall.js
@@ -1,23 +1,24 @@
 import { Component, useState } from "react";
-import { Dropdown } from "react-bootstrap";
+
 import "./admin.css";
 import AdminNav from "./AdminNav";
-
+import axios from "axios";
 class Addhall extends Component {
     constructor() {
         super()
         this.state = {
-            hallId: '',
-            hallDesc: '',
-            hallCap: '',
+            hallId: 0,
+            hallDesc: "",
+            hallCap: 0,
 
 
         }
-
+        this.change = this.change.bind(this);
     }
 
-
-
+    change(event) {
+        this.setState({ hallDesc: event.target.value })
+    }
 
     render() {
         return (
@@ -25,29 +26,71 @@ class Addhall extends Component {
                 <AdminNav />
                 <div className="admin">
 
-                    <form>
-                        <h1>Add Hall</h1>
-                        <div className="formInput">
-                            <label>Add Hall ID</label>
-                            <input type="text" required />
 
-                            <select className="mt-2" name="hall" id="hall">
-                                <option value="volvo">Normal</option>
-                                <option value="saab">Delex</option>
-                                <option value="opel">Primim</option>
+                    <h1>Add Hall</h1>
+                    <div className="formInput">
+                        <label>Add Hall ID</label>
+                        <input type="number" required onChange={(e) => {
+                            this.setState({ hallId: e.target.value })
+                        }} />
 
-                            </select>
+                        {/* <select className="mt-2" name="hall" id="hall">
+                                <option value="volvo" onChange={()=>{
+                                  
+                                   this.state.hallDesc="Normal"
+                                   this.setState({hallDesc:"Normal"})
+                                }}>Normal</option>
+                                <option value="saab"onChange={()=>{
+                                  // this.setState({hallDesc:e.target.value})
+                                  this.state.hallDesc="Delux"
+                                  this.setState({hallDesc:"Delux"})
+                                }}>Delex</option>
+                                <option value="opel" onChange={()=>{
+                              
+                                   this.state.hallDesc="Premium"
+                                   this.setState({hallDesc:"Premium"})
+                                }}>Primim</option>
 
-                            <label>Total capacity</label>
-                            <input type="text" required />
+                            </select> */}
 
-                            <button type="submit">Add </button>
-                        </div>
-                    </form>
+                        <label>Hall Description</label>
+                        <select id="lang" onChange={this.change} value={this.state.value}>,
+                            <option value="select" selected >select</option>
+                            <option value="Normal" >Normal</option>
+                            <option value="deluxe">deluxe</option>
+                            <option value="primium">Primium</option>
+                        </select>
+
+                        <label>Total capacity</label>
+                        <input type="number" required onChange={(e) => {
+                            this.setState({ hallCap: e.target.value })
+                        }} />
+
+                        <button type="submit" onClick={() => {
+                            var addHall = {
+                                hallId: this.state.hallId,
+                                hallDesc: this.state.hallDesc,
+                                totalCapacity: this.state.hallCap
+                            }
+                            console.log(addHall);
+                            var option = {
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }
+                            }
+                            axios.post("http://localhost:8080/addhall", addHall, option).then((res) => {
+                                console.log(res.data)
+                            }).catch((err) => {
+                                console.log("error has occured")
+                            })
+                        }}>Add </button>
+                    </div>
+
                 </div>
             </>
 
         );
     }
 }
+
 export default Addhall;
--- src/admin/AdminHome.js
+++ src/admin/AdminHome.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react'
-import GetAllShows from '../Componenets/MovieShow/GetAllShows'
+import React from 'react'
 import AdminNav from './AdminNav'
 
-export default class AdminHome extends Component {
-    render() {
-        return (
-            <div>
-                <AdminNav />
+export default function AdminHome() {
+    var ui = (
+        <div>
+            <AdminNav />
+            <div className='app'>
+                <center>
+                    <h1 class='header'>Welcome Back Admin</h1>
+                    <br /><br />
 
-
+                </center>
             </div>
-        )
+
+        </div>
+    )
+    var loggedUi = (
+        <h1>You have logged out</h1>
+    );
+    if (localStorage.getItem('email') == null) {
+        return loggedUi;
     }
+
+    else {
+
+        return ui;
+
+    }
+
 }
